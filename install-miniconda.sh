a8f3102bf697ab74c21d7f18529e6bf06725e731
DuncanMcGregor <duncan@oneeyedmen.com>
2022-01-12 15:25:57 +0000

    things

---
+++ install-miniconda.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b
+rm Miniconda3-latest-Linux-x86_64.sh
+source ~/miniconda3/bin/activate
+conda init
